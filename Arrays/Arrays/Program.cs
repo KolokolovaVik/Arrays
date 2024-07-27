147e7390aff63f894319507952c90293535f66e3
Kolokolova_Vik <2645436@gmail.com>
2024-07-27 09:48:39 +0300

    Commit1

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sectors = { 1, 6, 16, 28, 119 };
+
+            while (true)
+            {
+                for (int i = 0; i < sectors.Length; i++)
+                {
+                    Console.WriteLine($"В секторе {i+1}: {sectors[i]} мест");
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
