65eeb7e43f753ec961d01b70a3b0a04fd8045a81
Kolokolova_Vik <2645436@gmail.com>
2024-07-31 19:06:01 +0300

    Homework 3.1

---
+++ Arrays/Arrays/Homework31.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Homework31
+    {
+        static void Main(string[] args)
+        {
+            int sumSecondLine = 0;
+            int multiplicationFirstColumn = 1;
+
+            int[,] array =
+            {
+                {3,9,5,9,2},
+                {4,6,7,1,2},
+                {5,7,9,2,3},
+            };
+
+            Console.WriteLine("Двумерный массив:\n");
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (i == 1)
+                    {
+                        sumSecondLine += array[i, j];
+                    }
+
+                    if (j == 0)
+                    {
+                        multiplicationFirstColumn *= array[i, j];
+                    }
+
+                    Console.Write(array[i, j]);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Сумма второй строки: " + sumSecondLine);
+            Console.WriteLine("Произведение первого столбца: " + multiplicationFirstColumn);
+        }
+    }
+}
--- Arrays/Arrays/Program.cs
+++ Arrays/Arrays/Program.cs
@@ -1,27 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace Arrays
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] sectors = { 1, 6, 16, 28, 119 };
+//namespace Arrays
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] numbers = { 1, 2, 3, 4 };
+//            foreach (int i in numbers)
+//            {
+//                Console.WriteLine(i);
+//            }
+//            //string greeting = "Привет!";
+//            //Console.WriteLine(greeting[6]);
 
-            while (true)
-            {
-                for (int i = 0; i < sectors.Length; i++)
-                {
-                    Console.WriteLine($"В секторе {i+1}: {sectors[i]} мест");
-                }
+//            //    char[] bag = new char[1];
+//            //    int userX = 10, userY = 11;
+//            //    Console.CursorVisible = false;
 
-                Console.ReadKey();
-                Console.Clear();
-            }
-        }
-    }
-}
+//            //    char[,] map =
+//            //    {
+//            //        {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+//            //        {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ','X',' ',' ',' ',' ',' ','X',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ','#',' ',' ',' ',' ','#',' ',' ','#',' ',' ',' ','#'},
+//            //        {'#',' ','#',' ',' ',' ',' ','#',' ',' ','#',' ',' ',' ','#'},
+//            //        {'#',' ','#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ','#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ','X',' ','#'},
+//            //        {'#',' ',' ',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ','#',' ',' ',' ',' ',' ',' ','X',' ',' ',' ','#'},
+//            //        {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#','X',' ',' ',' ','#',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','#'},
+//            //        {'#','#','#','#','#','#','#','#','#','#','#','#','#','#','#'},
+//            //    };
+
+//            //    while (true)
+//            //    {
+//            //        Console.SetCursorPosition(0, 20);
+//            //        Console.Write("Сумка: ");
+//            //        for (int i = 0; i < bag.Length; i++)
+//            //        {
+//            //            Console.Write(bag[i] + " ");
+//            //        }
+
+//            //        Console.SetCursorPosition(0, 0);
+
+//            //        for (int i = 0; i < map.GetLength(0); i++)
+//            //        {
+//            //            for (int j = 0; j < map.GetLength(1); j++)
+//            //            {
+//            //                Console.Write(map[i, j]);
+//            //            }
+//            //            Console.WriteLine();
+//            //        }
+
+//            //        Console.SetCursorPosition(userX, userY);
+//            //        Console.Write('@');
+
+//            //        ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+//            //        switch (keyInfo.Key)
+//            //        {
+//            //            case ConsoleKey.UpArrow:
+//            //                if (map[userY - 1, userX] != '#')
+//            //                {
+//            //                    userY--;
+//            //                }
+//            //                break;
+
+//            //            case ConsoleKey.DownArrow:
+//            //                if (map[userY + 1, userX] != '#')
+//            //                {
+//            //                    userY++;
+//            //                }
+//            //                break;
+
+//            //            case ConsoleKey.LeftArrow:
+//            //                if (map[userY, userX - 1] != '#')
+//            //                {
+//            //                    userX--;
+//            //                }
+//            //                break;
+
+//            //            case ConsoleKey.RightArrow:
+//            //                if (map[userY, userX + 1] != '#')
+//            //                {
+//            //                    userX++;
+//            //                }
+//            //                break;
+//            //        }
+
+//            //        if (map[userY, userX] == 'X')
+//            //        {
+//            //            map[userY, userX] = 'o';
+//            //            char[] tempBag = new char[bag.Length + 1];
+//            //            for (int i = 0; i < bag.Length; i++)
+//            //            {
+//            //                tempBag[i] = bag[i];
+//            //            }
+//            //            tempBag[tempBag.Length - 1] = 'X';
+//            //            bag = tempBag;
+//            //        }
+//            //        Console.Clear();
+
+
+
+
+
+//            //    }
+//        }
+
+//    }
+//}
