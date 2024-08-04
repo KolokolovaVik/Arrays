e2136efa3bf7464350bf308f2e7cd09185ff5494
Kolokolova_Vik <2645436@gmail.com>
2024-08-04 13:43:20 +0300

    Homework3.4

--- Arrays/Arrays/Homework33.cs
+++ Arrays/Arrays/Homework33.cs
@@ -1,45 +1,45 @@
-﻿using System;
+﻿//using System;
 
-namespace Arrays
-{
-    internal class Homework33
-    {
-        static void Main(string[] args)
-        {
-            Random random = new Random();
+//namespace Arrays
+//{
+//    internal class Homework33
+//    {
+//        static void Main(string[] args)
+//        {
+//            Random random = new Random();
 
-            int sizeArray = 30;
-            int randomNumber = 100;
+//            int sizeArray = 30;
+//            int randomNumber = 100;
 
-            int[] numbers = new int[sizeArray];
+//            int[] numbers = new int[sizeArray];
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbers[i] = random.Next(randomNumber);
-                Console.Write(numbers[i] + "|");
-            }
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                numbers[i] = random.Next(randomNumber);
+//                Console.Write(numbers[i] + "|");
+//            }
 
-            Console.WriteLine();
+//            Console.WriteLine();
 
-            if (numbers[0] > numbers[1])
-            {
-                Console.WriteLine("\nЛокальный максимум: " + numbers[0]);
-            }
+//            if (numbers[0] > numbers[1])
+//            {
+//                Console.WriteLine("\nЛокальный максимум: " + numbers[0]);
+//            }
 
-            for (int i = 1; i < numbers.Length - 1; i++)
-            {
-                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
-                {
-                    Console.WriteLine("\nЛокальный максимум: " + numbers[i]);
-                }
-            }
+//            for (int i = 1; i < numbers.Length - 1; i++)
+//            {
+//                if (numbers[i] > numbers[i - 1] && numbers[i] > numbers[i + 1])
+//                {
+//                    Console.WriteLine("\nЛокальный максимум: " + numbers[i]);
+//                }
+//            }
 
-            if (numbers[numbers.Length - 1] > numbers[numbers.Length - 2])
-            {
-                Console.WriteLine("\nлокальный максимум: " + numbers[numbers.Length - 1]);
-            }
-        }
-    }
-}
+//            if (numbers[numbers.Length - 1] > numbers[numbers.Length - 2])
+//            {
+//                Console.WriteLine("\nлокальный максимум: " + numbers[numbers.Length - 1]);
+//            }
+//        }
+//    }
+//}
 
 
---
+++ Arrays/Arrays/Homework34.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Arrays
+{
+    internal class Homework34
+    {
+        static void Main(string[] args)
+        {
+            const string CommandSum = "Sum";
+            const string CommandExit = "Exit";
+
+            int[] numbers = new int[1];
+
+            bool isWork = true;
+
+            while (isWork)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.Write("Пользователь ввёл числа: ");
+
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    Console.Write(numbers[i] + "|");
+                }
+
+                int[] tempNumbrs = new int[numbers.Length + 1];
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    tempNumbrs[i] = numbers[i];
+                }
+
+                Console.WriteLine("\nВведите число: ");
+                tempNumbrs[numbers.Length - 1] = Convert.ToInt32(Console.ReadLine());
+                numbers = tempNumbrs;
+
+                Console.SetCursorPosition(0, 5);
+                Console.WriteLine($"Введите команду:\n{CommandSum} - для вывода суммы введённых чисел. " +
+                    $"\n{CommandExit} - для выхода из программы.");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case CommandSum:
+                        int sum = 0;
+                        for (int i = 0; i < numbers.Length; i++)
+                        {
+                            sum += numbers[i];
+                        }
+                        Console.WriteLine($"\nСумма всех чисел: {sum}");
+                        break;
+
+                    case CommandExit:
+                        isWork = false;
+                        Console.WriteLine("Команда завершена.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Такой команды нет");
+                        break;
+
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
