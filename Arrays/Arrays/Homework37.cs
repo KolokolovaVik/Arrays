6984de0b17d82bf25b15430a54a13a1dc941a46a
Kolokolova_Vik <2645436@gmail.com>
2024-08-07 21:29:24 +0300

    Homework3.8

--- Arrays/Arrays/Homework37.cs
+++ Arrays/Arrays/Homework37.cs
@@ -1,19 +1,19 @@
-﻿using System;
+﻿//using System;
 
-namespace Arrays
-{
-    internal class Homework37
-    {
-        static void Main(string[] args)
-        {
-            string text = "Split это город в Хорватии.";
+//namespace Arrays
+//{
+//    internal class Homework37
+//    {
+//        static void Main(string[] args)
+//        {
+//            string text = "Split это город в Хорватии.";
 
-            string[] substring = text.Split();
+//            string[] substring = text.Split();
 
-            foreach (string sub in substring)
-            {
-                Console.WriteLine(sub);
-            }
-        }
-    }
-}
+//            foreach (string sub in substring)
+//            {
+//                Console.WriteLine(sub);
+//            }
+//        }
+//    }
+//}
--- Arrays/Arrays/Homework38.cs
+++ Arrays/Arrays/Homework38.cs
@@ -1,34 +1,29 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
 
-//namespace Arrays
-//{
-//    internal class Homework38
-//    {
+namespace Arrays
+{
+    internal class Homework38
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-//        static void Main(string[] args)
-//        {
-//            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine("Насколько элементов вы хотите сдвинуть массив влево?");
+            int shift = Convert.ToInt32(Console.ReadLine());
 
-//            Console.WriteLine("Насколько элементов вы хотите сдвинуть массив влево?");
-//            int shift = Convert.ToInt32(Console.ReadLine());
+            for (int j = 0; j < numbers.Length - shift; j++)
+            {
+                int tempVariable = numbers[j];
+                numbers[j] = numbers[j + shift];
+                numbers[j + shift] = tempVariable;
+            }
 
-//            for (int j = 0; j < numbers.Length - shift; j++)
-//            {
-//                int temp = numbers[j];
-//                numbers[j] = numbers[j + shift];
-//                numbers[j + shift] = temp;
-//            }
+            Console.WriteLine($"Массив со сдвигом влево на: {shift}.");
 
-//            Console.WriteLine($"Массив со сдвигом влево на: {shift}.");
-
-//            for (int i = 0; i < numbers.Length; i++)
-//            {
-//                Console.Write(numbers[i] + " ");
-//            }
-//        }
-//    }
-//}
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
