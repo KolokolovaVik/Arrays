e1039f560b720e774534b8160b861b87d94eda57
Kolokolova_Vik <2645436@gmail.com>
2024-08-07 15:56:14 +0300

    Homework 3.6

--- Arrays/Arrays/Homework35.cs
+++ Arrays/Arrays/Homework35.cs
@@ -1,48 +1,48 @@
-﻿using System;
+﻿//using System;
 
-namespace Arrays
-{
-    internal class Homework35
-    {
-        static void Main(string[] args)
-        {
-            int sizeArray = 30;
-            int counter = 1;
-            int maxCountRepetitions = 0;
-            int number = 0;
-            int repeatableNumber = 0;
-            int maxNumberRandom = 10;
+//namespace Arrays
+//{
+//    internal class Homework35
+//    {
+//        static void Main(string[] args)
+//        {
+//            int sizeArray = 30;
+//            int counter = 1;
+//            int maxCountRepetitions = 0;
+//            int number = 0;
+//            int repeatableNumber = 0;
+//            int maxNumberRandom = 10;
 
-            int[] numbers = new int[sizeArray];
+//            int[] numbers = new int[sizeArray];
 
-            Random random = new Random();
+//            Random random = new Random();
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbers[i] = random.Next(maxNumberRandom);
-                Console.Write(numbers[i] + " ");
-            }
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                numbers[i] = random.Next(maxNumberRandom);
+//                Console.Write(numbers[i] + " ");
+//            }
 
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                if (numbers[i] == numbers[i + 1])
-                {
-                    counter++;
-                    number = numbers[i];
-                }
-                else
-                {
-                    counter = 1;
-                }
+//            for (int i = 0; i < numbers.Length - 1; i++)
+//            {
+//                if (numbers[i] == numbers[i + 1])
+//                {
+//                    counter++;
+//                    number = numbers[i];
+//                }
+//                else
+//                {
+//                    counter = 1;
+//                }
 
-                if (maxCountRepetitions < counter)
-                {
-                    maxCountRepetitions = counter;
-                    repeatableNumber = number;
-                }
-            }
+//                if (maxCountRepetitions < counter)
+//                {
+//                    maxCountRepetitions = counter;
+//                    repeatableNumber = number;
+//                }
+//            }
 
-            Console.WriteLine($"\nЧисло: {repeatableNumber} повторяется {maxCountRepetitions} раз");
-        }
-    }
-}
+//            Console.WriteLine($"\nЧисло: {repeatableNumber} повторяется {maxCountRepetitions} раз");
+//        }
+//    }
+//}
--- Arrays/Arrays/Homework36.cs
+++ Arrays/Arrays/Homework36.cs
@@ -1,39 +1,45 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
 
-//namespace Arrays
-//{
-//    internal class Homework36
-//    {
-//        static void Main(string[] args)
-//        {
-//            int[] numbers = new int[30];
-//            Random ra = new Random();
-//            for (int i = 0; i < numbers.Length; i++)
-//            {
-//                numbers[i] = ra.Next(100);
-//            }
+namespace Arrays
+{
+    internal class Homework36
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
 
-//            for (int i = 0; i < numbers.Length - 1; i++)
-//            {
-//                for (int j = 0; j< numbers.Length - 1; j++)
-//                    if (numbers[j] > numbers[j +1])
-//                    {
-//                        int temp = numbers[j + 1];
-//                        numbers[j  + 1] = numbers[j];
-//                        numbers[j] = temp;
-//                    }
+            int sizeArray = 10;
+            int randomNumber = 100;
 
+            int[] numbers = new int[sizeArray];
 
-//            }
+            Console.WriteLine("Массив:");
 
-//            for (int i = 0; i < numbers.Length; i++)
-//            {
-//                Console.Write(numbers[i] + " ");
-//            }
-//        }
-//    }
-//}
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(randomNumber);
+                Console.Write(numbers[i] + " ");
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < numbers.Length - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int tempNumber = numbers[j + 1];
+                        numbers[j + 1] = numbers[j];
+                        numbers[j] = tempNumber;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nОтсортированный массив от меньшего к большему:");
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
