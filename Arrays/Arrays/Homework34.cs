8e0b21b366876e160ecece42e5c602d72531188b
Kolokolova_Vik <2645436@gmail.com>
2024-08-07 13:32:32 +0300

    Homework3.5

--- Arrays/Arrays/Homework34.cs
+++ Arrays/Arrays/Homework34.cs
@@ -1,68 +1,68 @@
-﻿using System;
+﻿//using System;
 
-namespace Arrays
-{
-    internal class Homework34
-    {
-        static void Main(string[] args)
-        {
-            const string CommandSum = "Sum";
-            const string CommandExit = "Exit";
+//namespace Arrays
+//{
+//    internal class Homework34
+//    {
+//        static void Main(string[] args)
+//        {
+//            const string CommandSum = "Sum";
+//            const string CommandExit = "Exit";
 
-            int[] numbers = new int[0];
+//            int[] numbers = new int[0];
 
-            bool isWork = true;
+//            bool isWork = true;
 
-            while (isWork)
-            {
-                Console.SetCursorPosition(0, 0);
-                Console.Write("Пользователь ввёл числа: ");
+//            while (isWork)
+//            {
+//                Console.SetCursorPosition(0, 0);
+//                Console.Write("Пользователь ввёл числа: ");
 
-                for (int i = 0; i < numbers.Length; i++)
-                {
-                    Console.Write(numbers[i] + "|");
-                }
+//                for (int i = 0; i < numbers.Length; i++)
+//                {
+//                    Console.Write(numbers[i] + "|");
+//                }
 
-                Console.SetCursorPosition(0, 5);
-                Console.WriteLine($"Введите команду:\n{CommandSum} - для вывода суммы введённых чисел. " +
-                    $"\n{CommandExit} - для выхода из программы.");
-                string userInput = Console.ReadLine();
+//                Console.SetCursorPosition(0, 5);
+//                Console.WriteLine($"Введите команду:\n{CommandSum} - для вывода суммы введённых чисел. " +
+//                    $"\n{CommandExit} - для выхода из программы.");
+//                string userInput = Console.ReadLine();
 
-                switch (userInput)
-                {
-                    case CommandSum:
-                        int sum = 0;
+//                switch (userInput)
+//                {
+//                    case CommandSum:
+//                        int sum = 0;
 
-                        for (int i = 0; i < numbers.Length; i++)
-                        {
-                            sum += numbers[i];
-                        }
+//                        for (int i = 0; i < numbers.Length; i++)
+//                        {
+//                            sum += numbers[i];
+//                        }
 
-                        Console.WriteLine($"\nСумма всех чисел: {sum}");
-                        break;
+//                        Console.WriteLine($"\nСумма всех чисел: {sum}");
+//                        break;
 
-                    case CommandExit:
-                        isWork = false;
-                        Console.WriteLine("Программа завершена.");
-                        break;
+//                    case CommandExit:
+//                        isWork = false;
+//                        Console.WriteLine("Программа завершена.");
+//                        break;
 
-                    default:
-                        int[] tempNumbrs = new int[numbers.Length + 1];
+//                    default:
+//                        int[] tempNumbrs = new int[numbers.Length + 1];
 
-                        for (int i = 0; i < numbers.Length; i++)
-                        {
-                            tempNumbrs[i] = numbers[i];
-                        }
+//                        for (int i = 0; i < numbers.Length; i++)
+//                        {
+//                            tempNumbrs[i] = numbers[i];
+//                        }
 
-                        tempNumbrs[tempNumbrs.Length - 1] = Convert.ToInt32(userInput);
-                        numbers = tempNumbrs;
-                        break;
-                }
+//                        tempNumbrs[tempNumbrs.Length - 1] = Convert.ToInt32(userInput);
+//                        numbers = tempNumbrs;
+//                        break;
+//                }
 
-                Console.WriteLine("Нажмите любую клавишу.");
-                Console.ReadKey();
-                Console.Clear();
-            }
-        }
-    }
-}
+//                Console.WriteLine("Нажмите любую клавишу.");
+//                Console.ReadKey();
+//                Console.Clear();
+//            }
+//        }
+//    }
+//}
---
+++ Arrays/Arrays/Homework36.cs
@@ -0,0 +1,39 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Arrays
+//{
+//    internal class Homework36
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] numbers = new int[30];
+//            Random ra = new Random();
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                numbers[i] = ra.Next(100);
+//            }
+
+//            for (int i = 0; i < numbers.Length - 1; i++)
+//            {
+//                for (int j = 0; j< numbers.Length - 1; j++)
+//                    if (numbers[j] > numbers[j +1])
+//                    {
+//                        int temp = numbers[j + 1];
+//                        numbers[j  + 1] = numbers[j];
+//                        numbers[j] = temp;
+//                    }
+
+
+//            }
+
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                Console.Write(numbers[i] + " ");
+//            }
+//        }
+//    }
+//}
