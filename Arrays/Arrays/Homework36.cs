3a877d2d4db4908ca2bb13e399420bbe0318ca0b
Kolokolova_Vik <2645436@gmail.com>
2024-08-07 18:38:48 +0300

    Homework 3.7

--- Arrays/Arrays/Homework36.cs
+++ Arrays/Arrays/Homework36.cs
@@ -1,45 +1,45 @@
-﻿using System;
+﻿//using System;
 
-namespace Arrays
-{
-    internal class Homework36
-    {
-        static void Main(string[] args)
-        {
-            Random random = new Random();
+//namespace Arrays
+//{
+//    internal class Homework36
+//    {
+//        static void Main(string[] args)
+//        {
+//            Random random = new Random();
 
-            int sizeArray = 10;
-            int randomNumber = 100;
+//            int sizeArray = 10;
+//            int randomNumber = 100;
 
-            int[] numbers = new int[sizeArray];
+//            int[] numbers = new int[sizeArray];
 
-            Console.WriteLine("Массив:");
+//            Console.WriteLine("Массив:");
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbers[i] = random.Next(randomNumber);
-                Console.Write(numbers[i] + " ");
-            }
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                numbers[i] = random.Next(randomNumber);
+//                Console.Write(numbers[i] + " ");
+//            }
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                for (int j = 0; j < numbers.Length - i - 1; j++)
-                {
-                    if (numbers[j] > numbers[j + 1])
-                    {
-                        int tempNumber = numbers[j + 1];
-                        numbers[j + 1] = numbers[j];
-                        numbers[j] = tempNumber;
-                    }
-                }
-            }
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                for (int j = 0; j < numbers.Length - i - 1; j++)
+//                {
+//                    if (numbers[j] > numbers[j + 1])
+//                    {
+//                        int tempNumber = numbers[j + 1];
+//                        numbers[j + 1] = numbers[j];
+//                        numbers[j] = tempNumber;
+//                    }
+//                }
+//            }
 
-            Console.WriteLine("\nОтсортированный массив от меньшего к большему:");
+//            Console.WriteLine("\nОтсортированный массив от меньшего к большему:");
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                Console.Write(numbers[i] + " ");
-            }
-        }
-    }
-}
+//            for (int i = 0; i < numbers.Length; i++)
+//            {
+//                Console.Write(numbers[i] + " ");
+//            }
+//        }
+//    }
+//}
